4a285b4c54c45d3d9883c133b05ba74818c3b44f
謝 <xies4007@gmail.com>
2022-12-10 14:40:03 +0800

    (test)加上卡方、t檢定的中文解釋

--- 中醫體質_LS305.R
+++ 中醫體質_LS305.R
@@ -1,13 +1,13 @@
 library(tidyverse)
-
+#kfnkefjkefjekjfkejfklfke
 #抽取資料---------------------------------------------------------------------------------------------------------------------------------
-#生菌讀取路徑:C:\\R\\      威甫讀取路徑:C:\\R\\LS305中醫\\
+#生菌讀取路徑:C:\\R\\      威甫讀取路徑:C:\\R\\LS305中醫
 #各項體檢資料
-measure <- read.csv("C:\\R\\LS305中醫\\release_list_measure.csv",sep='\t', header=TRUE,na = "NA",fileEncoding = "Big5")
+measure <- read.csv("C:\\R\\release_list_measure.csv",sep=",", header=TRUE,na = "NA")
 #各種體質資料
-TCMlist<- read.csv("C:\\R\\LS305中醫\\TCM_list20220924.csv",fileEncoding = "Big5")
+TCMlist<- read.csv("C:\\R\\TCM_list20220924.csv",fileEncoding = "Big5")
 ##體質跟各項體檢資料
-TCMcal <- read.csv("C:\\R\\LS305中醫\\TCMmerge3.csv",fileEncoding = "Big5")
+TCMcal <- read.csv("C:\\R\\TCMmerge3.csv",fileEncoding = "Big5")
 
 
 #分離資料---------------------------------------------------------------------------------------------------------------------------------
@@ -47,8 +47,8 @@ table(age,sex)
 #匯出各體質男女人數&各年齡體質人數 csv檔
 aaa <- table(con,sex)
 bbb <- table(age,sex)
-write.csv(aaa, file = "C:\\R\\LS305中醫\\各體質男女人數.csv",fileEncoding = "Big5")
-write.csv(bbb, file = "C:\\R\\LS305中醫\\各年齡體質人數.csv",fileEncoding = "Big5")
+write.csv(aaa, file = "C:\\R\\各體質男女人數.csv",fileEncoding = "Big5")
+write.csv(bbb, file = "C:\\R\\各年齡體質人數.csv",fileEncoding = "Big5")
 
 #取體檢取特定欄位---------------------------------------------------------------------------------------------------------------------------------------
 
@@ -59,20 +59,16 @@ measure_extract<-subset(measure,
                                    "SIT_2_SYSTOLIC_PRESSURE","SIT_2_DIASTOLIC_PRESSURE",
                                    "SIT_3_SYSTOLIC_PRESSURE","SIT_3_DIASTOLIC_PRESSURE",
                                    "SIT_1_HEARTBEAT_SPEED","SIT_2_HEARTBEAT_SPEED","SIT_3_HEARTBEAT_SPEED",
-                                   "BONE_EXAM_RESULT","T_SCORE","Z_SCORE","SOS","BUA",
-                                   "VC","VC_PRED","TV","ERV","IRV","IC","VC_HT","FVC","FVC_PRED",
+                                   "BONE_EXAM_RESULT","T_SCORE","Z_SCORE",
+                                   "VC","TV","ERV","IRV","IC","VC_HT","FVC","FVC_PRED",
                                    "FEV10","FEV10_PRED","FEV10_FVC","FEV10_FVC_PRED","FEV10_SVC",
-                                   "FEV10_SVC_PRED","FEV10_SVC_P_PRED","FEV10_VCPR","MMF","MMF_PRED",
-                                   "PEF","PEF_PRED","FEF25","FEF25_PRED","FEF50","FEF50_PRED","FEF75","FEF75_PRED","FEF75_HT","FEF75_HT_PRED",
-                                   "EXTRAPV_P","FIV10_FVC","MVV",
                                    "RBC","WBC","PLATELET","HB","HCT","HBA1C",
                                    "ANTI_HCV_AB_1","ANTI_HCV_AB_2","HBSAG_1","HBSAG_2","HBEAG_1","HBEAG_2",
                                    "ANTI_HBS_AB_1","ANTI_HBS_AB_2","ANTI_HBC_AB_1","ANTI_HBC_AB_2","ANTI_HDV_AB_1","ANTI_HDV_AB_2",
                                    "FASTING_GLUCOSE","T_CHO","TG","HDL_C","LDL_C",
                                    "T_BILIRUBIN","ALBUMIN","SGOT","SGPT","GAMMA_GT","AFP",
                                    "BUN","CREATININE","URIC_ACID","MICROALB","CREATININE_URINE")
-                        )
-
+)
 
 #抽取中醫體質的"Release_No","體質","SEX","AGE","age_gruop"和release_list_measure.csv依照 "Release_No"合併------------------------------------------------
 TCMlist<-subset(TCMlist,select=c("Release_No","體質","SEX","AGE","age_gruop"))
@@ -81,7 +77,7 @@ TCMmerge<-merge(TCMlist,measure_extract, by = "Release_No", all.TCMlist = T)
 
 
 #TCMmerge合併TWB1,2序號
-TWB12 <- read.csv("C:\\R\\LS305中醫\\lab_info.csv",fileEncoding = "Big5")
+TWB12 <- read.csv("C:\\R\\lab_info.csv",fileEncoding = "Big5")
 names(TWB12)[1] <- "Release_No"
 TWB12 <- subset(TWB12,select=c("Release_No","TWB1_ID","TWB2_ID"))
 TCMmerge2<-merge(TWB12,TCMmerge, by = "Release_No", all.TCMmerge = T)
@@ -92,7 +88,7 @@ TCMmerge3 <- distinct(TCMmerge2, BMI,BODY_FAT_RATE,BODY_WAISTLINE,BODY_BUTTOCKS,
                       SIT_2_SYSTOLIC_PRESSURE,VC,TV,ERV,
                       .keep_all = TRUE )
 
-write.csv(TCMmerge3,file='C:\\R\\LS305中醫\\TCMmerge3.csv',fileEncoding = "Big5",append = FALSE)
+
 
 #只取baseline
 chiqTCM <- subset(TCMcal,
@@ -100,90 +96,102 @@ chiqTCM <- subset(TCMcal,
 #替換文字成數字
 #中醫體質
 chiqTCM$體質 <- as.character(chiqTCM$體質)
-  
-chiqTCM$體質[which(chiqTCM$體質=="平和")] <- "1"
-chiqTCM$體質[which(chiqTCM$體質=="陰虛")] <- "2"
-chiqTCM$體質[which(chiqTCM$體質=="陰虛+陽虛")] <- "3"
-chiqTCM$體質[which(chiqTCM$體質=="陰虛+陽虛+痰瘀")] <- "4"
-chiqTCM$體質[which(chiqTCM$體質=="陰虛+痰瘀")] <- "5"
-chiqTCM$體質[which(chiqTCM$體質=="陽虛")] <- "6"
-chiqTCM$體質[which(chiqTCM$體質=="痰瘀")] <- "7"
-chiqTCM$體質[which(chiqTCM$體質=="痰瘀+陽虛")] <- "8"
+
+chiqTCM$體質[which(chiqTCM$體質=="平和")] <- 1
+chiqTCM$體質[which(chiqTCM$體質=="陰虛")] <- 2
+chiqTCM$體質[which(chiqTCM$體質=="陰虛+陽虛")] <- 3
+chiqTCM$體質[which(chiqTCM$體質=="陰虛+陽虛+痰瘀")] <- 4
+chiqTCM$體質[which(chiqTCM$體質=="陰虛+痰瘀")] <- 5
+chiqTCM$體質[which(chiqTCM$體質=="陽虛")] <- 6
+chiqTCM$體質[which(chiqTCM$體質=="痰瘀")] <- 7
+chiqTCM$體質[which(chiqTCM$體質=="痰瘀+陽虛")] <- 8
 chiqTCM$體質 <- as.numeric(chiqTCM$體質)
-  
+
 #ANTI_HCV_AB_1
 chiqTCM$ANTI_HCV_AB_1 <- as.character(chiqTCM$ANTI_HCV_AB_1)
 chiqTCM$ANTI_HCV_AB_1[which(chiqTCM$ANTI_HCV_AB_1=="Positive")] <- "1"
 chiqTCM$ANTI_HCV_AB_1[which(chiqTCM$ANTI_HCV_AB_1=="Negative")] <- "2"
-  
+
 #HBSAG_1
 chiqTCM$HBSAG_1 <- as.character(chiqTCM$HBSAG_1)
 chiqTCM$HBSAG_1[which(chiqTCM$HBSAG_1=="Positive")] <- "1"
 chiqTCM$HBSAG_1[which(chiqTCM$HBSAG_1=="Negative")] <- "2"
-  
+
 #HBEAG_1
 chiqTCM$HBEAG_1 <- as.character(chiqTCM$HBEAG_1)
 chiqTCM$HBEAG_1[which(chiqTCM$HBEAG_1=="Positive")] <- "1"
 chiqTCM$HBEAG_1[which(chiqTCM$HBEAG_1=="Negative")] <- "2"
-  
+
 #ANTI_HBS_AB_1
 chiqTCM$ANTI_HBS_AB_1 <- as.character(chiqTCM$ANTI_HBS_AB_1)
 chiqTCM$ANTI_HBS_AB_1[which(chiqTCM$ANTI_HBS_AB_1=="Positive")] <- "1"
 chiqTCM$ANTI_HBS_AB_1[which(chiqTCM$ANTI_HBS_AB_1=="Negative")] <- "2"
-  
+
 #ANTI_HBC_AB_1
 chiqTCM$ANTI_HBC_AB_1 <- as.character(chiqTCM$ANTI_HBC_AB_1)
 chiqTCM$ANTI_HBC_AB_1[which(chiqTCM$ANTI_HBC_AB_1=="Positive")] <- "1"
 chiqTCM$ANTI_HBC_AB_1[which(chiqTCM$ANTI_HBC_AB_1=="Negative")] <- "2"
-  
+
 #ANTI_HDV_AB_1
 chiqTCM$ANTI_HDV_AB_1 <- as.character(chiqTCM$ANTI_HDV_AB_1)
 chiqTCM$ANTI_HDV_AB_1[which(chiqTCM$ANTI_HDV_AB_1=="Positive")] <- "1"
 chiqTCM$ANTI_HDV_AB_1[which(chiqTCM$ANTI_HDV_AB_1=="Negative")] <- "2"
-  
+
+#(昇峻)輸出更改文字的檔案
+write.csv(chiqTCM,file='C:\\R\\chiqTCM.csv',fileEncoding = "Big5")
+#(威甫)輸出更改文字的檔案
 write.csv(chiqTCM,file='C:\\R\\LS305中醫\\chiqTCM.csv',fileEncoding = "Big5")
+
+#資料清洗-----------------------------------------------------------------
+chiqTCM[is.na(chiqTCM)] <- 0
+chiqTCM$HBA1C <- as.numeric(chiqTCM$HBA1C)
+chiqTCM$ANTI_HBS_AB_2 <- as.numeric(chiqTCM$ANTI_HBS_AB_2)
+chiqTCM$SGPT <- as.numeric(chiqTCM$SGPT)
+chiqTCM$GAMMA_GT <- as.numeric(chiqTCM$GAMMA_GT)
+chiqTCM$AFP <- as.numeric(chiqTCM$AFP)
+chiqTCM$MICROALB <- as.numeric(chiqTCM$MICROALB)
+chiqTCM$體質 <- as.numeric(chiqTCM$體質)
+for (i in length(use_T_test)){
+  chiqTCM$use_T_test[i] <- as.numeric(chiqTCM$use_T_test[i])
+  >>>>>>> main
   
+}
+
+
 #做卡方檢定---------------------------------------------------------------------------------------------------------------------------
 
-use_vb <- c("SEX", "AGE", 
-            "age_gruop","BODY_HEIGHT","BODY_WEIGHT","BMI","BODY_FAT_RATE","BODY_WAISTLINE","BODY_BUTTOCKS","WHR",
-            "SIT_1_SYSTOLIC_PRESSURE","SIT_1_DIASTOLIC_PRESSURE",
-            "SIT_2_SYSTOLIC_PRESSURE","SIT_2_DIASTOLIC_PRESSURE",
-            "SIT_3_SYSTOLIC_PRESSURE","SIT_3_DIASTOLIC_PRESSURE",
-            "SIT_1_HEARTBEAT_SPEED","SIT_2_HEARTBEAT_SPEED","SIT_3_HEARTBEAT_SPEED",
-            "BONE_EXAM_RESULT","T_SCORE","Z_SCORE","SOS","BUA",
-            "VC","VC_PRED","TV","ERV","IRV","IC","VC_HT","FVC","FVC_PRED",
-            "FEV10","FEV10_PRED","FEV10_FVC","FEV10_FVC_PRED","FEV10_SVC",
-            "FEV10_SVC_PRED","FEV10_SVC_P_PRED","FEV10_VCPR","MMF","MMF_PRED",
-            "PEF","PEF_PRED","FEF25","FEF25_PRED","FEF50","FEF50_PRED","FEF75","FEF75_PRED","FEF75_HT","FEF75_HT_PRED",
-            "EXTRAPV_P","FIV10_FVC",
-            "RBC","WBC","PLATELET","HB","HCT","HBA1C",
-            "ANTI_HCV_AB_1","ANTI_HCV_AB_2","HBSAG_1","HBSAG_2","HBEAG_1","HBEAG_2",
-            "ANTI_HBS_AB_1","ANTI_HBS_AB_2","ANTI_HBC_AB_1","ANTI_HBC_AB_2","ANTI_HDV_AB_1","ANTI_HDV_AB_2",
-            "FASTING_GLUCOSE","T_CHO","TG","HDL_C","LDL_C",
-            "T_BILIRUBIN","ALBUMIN","SGOT","SGPT","GAMMA_GT","AFP",
-            "BUN","CREATININE","URIC_ACID","MICROALB","CREATININE_URINE")
-use_meaning<- c("性別", "年齡", 
-                "年齡層","身高","體重","身體質量指數","體脂肪率","腰圍","臀圍","腰臀比",
-                "靜坐時第一次收縮壓","靜坐時第一次舒張壓",
-                "靜坐時第二次收縮壓","靜坐時第二次舒張壓",
-                "靜坐時第三次收縮壓","靜坐時第三次舒張壓",
-                "靜坐時第一次心跳","靜坐時第二次心跳","靜坐時第三次心跳",
-                "STIFFNESS INDEX骨硬度指數","T參數值","Z參數值","超音波速度","寬頻超音波遞減值",
-                "肺活量","肺活量基準值","一回換氣量","預備呼氣量","預備吸氣量",
-                "預備呼氣量+預備吸氣量","肺活量/身長","強制呼出時肺活量（努力性肺活量）","強制呼出時肺活量基準值",
-                "Zeor點至1秒經過時的呼出量（一秒量）","一秒量基準值","1秒率，(FEV1.0/FVC)*100","FEV10_FVC基準值","1秒率，(FEV1.0/VC)*100",
-                "FEV10_SVC基準值","FEV10_SVC基準值","(FEV1.0/VC_PRED)*100","FVC的75％到25％的平均量（最大中間呼氣量）","MMF基準值",
-                "最大呼氣流量","最大呼氣流量基準值","努力性肺活量75％時流量","FEF25基準值","努力性肺活量50％時流量","FEF50基準值",
-                "努力性肺活量25％時流量","FEF75基準值","FEF75/身長","FEF75_HT基準值",
-                "(Extrapolated Volume/FVC)*100","FIV1.0/FVC",
-                "紅血球","白血球","血小板","血紅素","血球比容","醣化血色素值",
-                "C型肝炎抗體","C型肝炎抗體（值）","B型肝炎表面抗原","B型肝炎表面抗原（值）","B型肝炎e抗原","B型肝炎e抗原(值)",
-                "B型肝炎表面抗體","B型肝炎表面抗體（值）","B型肝炎核心抗體","B型肝炎核心抗體（值）","D型肝炎抗體","D型肝炎抗體（值）",
-                "飯前血糖","總膽固醇","三酸甘油酯","高密度酯蛋白膽固醇","低密度酯蛋白膽固醇",
-                "總膽紅素","白蛋白","血清麩胺酸苯醋酸轉氨基酵素","血清麩胺酸丙酮酸轉氨基酵素","A58	γ －麩胺醯轉移酵素","甲型胎兒血清蛋白",
-                "血中尿素氮","肌酸酐","尿酸","尿中微蛋白","尿中肌酸酐")
-result_set <- data.frame(vb1 = NA, vb2 = NA, vb3 = NA, p_value = NA)
+use_vb <- c("SEX", 
+            "age_gruop", 
+            "ANTI_HCV_AB_1", "HBSAG_1", "HBEAG_1" 
+            , "ANTI_HBS_AB_1", "ANTI_HBC_AB_1" 
+            , "ANTI_HDV_AB_1")
+use_meaning<- c("性別",
+                "年齡層",
+                "C型肝炎抗體","B型肝炎表面抗原","B型肝炎e抗原",
+                "B型肝炎表面抗體","B型肝炎核心抗體",
+                "D型肝炎抗體")
+use_T_test <- c("AGE","BODY_HEIGHT","BODY_WEIGHT","BMI","BODY_FAT_RATE","BODY_WAISTLINE", "BODY_BUTTOCKS", "WHR", "SIT_1_SYSTOLIC_PRESSURE", 
+                "SIT_1_DIASTOLIC_PRESSURE", "SIT_2_SYSTOLIC_PRESSURE", "SIT_2_DIASTOLIC_PRESSURE", 
+                "SIT_3_SYSTOLIC_PRESSURE", "SIT_3_DIASTOLIC_PRESSURE", "SIT_1_HEARTBEAT_SPEED", 
+                "SIT_2_HEARTBEAT_SPEED", "SIT_3_HEARTBEAT_SPEED", "BONE_EXAM_RESULT", 
+                "T_SCORE", "Z_SCORE","VC", "TV", "ERV", "IRV", "IC", "VC_HT", 
+                "FVC", "FEV10", "RBC", "WBC", "PLATELET", "HB", "HCT", "HBA1C",
+                "ANTI_HCV_AB_2", "HBSAG_2","HBEAG_2", "ANTI_HBS_AB_2","ANTI_HBC_AB_2", "ANTI_HDV_AB_2", "FASTING_GLUCOSE", 
+                "T_CHO", "TG", "HDL_C", "LDL_C", "T_BILIRUBIN", "ALBUMIN", "SGOT", 
+                "SGPT", "GAMMA_GT", "AFP", "BUN", "CREATININE", "URIC_ACID", 
+                "MICROALB", "CREATININE_URINE")
+use_T_test_meaning <- c("年齡","身高","體重","身體質量指數","體脂肪率","腰圍", "臀圍", "腰臀比", "靜坐時第一次收縮壓", 
+                        "靜坐時第一次舒張壓", "靜坐時第二次收縮壓", "靜坐時第二次舒張壓", 
+                        "靜坐時第三次收縮壓", "靜坐時第三次舒張壓", "靜坐時第一次心跳", 
+                        "靜坐時第二次心跳", "靜坐時第三次心跳", "STIFFNESS INDEX骨硬度指數", 
+                        "T參數值", "Z參數值","肺活量", "一回換氣量", "預備呼氣量", "預備吸氣量", "預備呼氣量+預備吸氣量", "肺活量/身長", 
+                        "強制呼出時肺活量（努力性肺活量）", "Zeor點至1秒經過時的呼出量（一秒量）", "紅血球", "白血球", "血小板", "血紅素", "血球比容", "醣化血色素值",
+                        "AC型肝炎抗體（值）", "B型肝炎表面抗原（值）","B型肝炎e抗原(值)", "B型肝炎表面抗體（值）","B型肝炎核心抗體（值）", "D型肝炎抗體（值）", "飯前血糖", 
+                        "總膽固醇", "三酸甘油酯", "高密度酯蛋白膽固醇", "低密度酯蛋白膽固醇", "總膽紅素", "白蛋白", "血清麩胺酸苯醋酸轉氨基酵素", 
+                        "血清麩胺酸丙酮酸轉氨基酵素", "A58	γ －麩胺醯轉移酵素", "甲型胎兒血清蛋白", "血中尿素氮", "肌酸酐", "尿酸", 
+                        "尿中微蛋白", "尿中肌酸酐")
+
+result_set <- data.frame(vb1 = NA, vb2 = NA, vb2_釋義 = NA, p_value = NA)
 xxx1 <- chiqTCM
 for (i in 1:length(use_vb)) {
   result_set[i,1] <- "體質"
@@ -191,6 +199,28 @@ for (i in 1:length(use_vb)) {
   result_set[i,3] <- use_meaning[i]
   result_set[i,4] <- chisq.test(xxx1[,c("體質")],xxx1[,use_vb[i]])$p.value
 }
+
+xxx2 <- chiqTCM
+result_set_T_test <- data.frame(vb1 = NA, vb2 = NA, p_value = NA)
+for (i in 1:length(use_T_test)) {
+  result_set_T_test[i,1] <- "體質"
+  result_set_T_test[i,2] <- use_T_test[i]
+  result_set_T_test[i,3] <- use_T_test_meaning[i]
+  result_set_T_test[i,4] <- t.test(xxx2[,5],xxx2[,use_T_test[i]])$p.value
+}
+#--------------作質方圖
+for (i in 1:length(use_T_test)){
+  hist(x=xxx2[,use_T_test[i]], main=use_T_test[i],xlab=use_T_test[i], ylab="數值")
+}
 #匯出檔案---------------------------------------------------------------------------------------------------------------------------------
-write.csv(result_set,file='C:\\R\\LS305中醫\\卡方結果.csv',fileEncoding = "Big5",append = FALSE)
+#(昇峻)卡方結果<<<<<<< HEAD
+write.csv(result_set,file='C:\\R\\卡方結果.csv',fileEncoding = "Big5")
+#(威甫)卡方結果
+write.csv(result_set,file='C:\\R\\LS305中醫\\卡方結果.csv',fileEncoding = "Big5")
+#(威甫)T-test結果
+write.csv(result_set_T_test, file='C:\\R\\LS305中醫\\T-test.csv',fileEncoding = "Big5")
+#輸出清洗完成的資料
+write.csv(chiqTCM, file='C:\\R\\LS305中醫\\中醫體質清理完成之資料.csv',fileEncoding = "Big5")
+
+#??????(>>>>>>> main)
 
