7070c9bb5af00dc243214472dcc1ff9e32555843
謝 <xies4007@gmail.com>
2023-01-05 15:30:52 +0800

    anova p value項目加上中文解釋

--- 中醫體質_LS305.R
+++ 中醫體質_LS305.R
@@ -3,7 +3,9 @@ library(Hmisc) # install.packages("Hmisc") straight from CRAN. version 	4.7-2
 library(grid) # version 4.3.0
 library(gtable) # version 0.3.1
 library(gridExtra)
-#抽取資料---------------------------------------------------------------------------------------------------------------------------------
+library(openxlsx)
+
+#抽取資料----------------------------------------------------------------------------------------------------------------------------
 #生菌讀取路徑:C:\\R\\      威甫讀取路徑:C:\\R\\LS305中醫
 #各項體檢資料
 measure <- read.csv("C:\\R\\LS305中醫\\release_list_measure.csv",sep=",", header=TRUE,na = "NA")
@@ -157,6 +159,113 @@ for (i in length(use_T_test)){
   
 }
 
+  
+#Anova 資料清洗---------------------------------------------------------------------------------------------
+source("C:\\Github\\LS305Anova_table_export.R")
+
+TCM_group <- TCM_group[,-grep("AGE|age_gruop",colnames(TCM_group))] #去除sex AGE age_group
+TCM_Anova_mergerdata<- merge(TCM_group, chiqTCM, by = "Release_No",all.x = TRUE  )
+TCM_Anova <- TCM_Anova_mergerdata[,-grep("X|TWB1_ID|TWB2_ID|SEX.y|體質",colnames(TCM_Anova_mergerdata))]
+TCM_Anova <- cbind(TCM_Anova[,1:24], TCM_Anova_mergerdata$BONE_EXAM_RESULT, TCM_Anova[,25:ncol(TCM_Anova)])
+TCM_Anova <- TCM_Anova[-6244,]
+
+#隔離無法ANOVA的資料
+TCM_Anova_failed <- subset(TCM_Anova,
+                           select = c("MVV","ANTI_HCV_AB_1","HBSAG_1","HBEAG_1",
+                                      "ANTI_HBS_AB_1","ANTI_HBC_AB_1","ANTI_HDV_AB_1")
+)
+TCM_Anova <- TCM_Anova[,-grep("MVV|ANTI_HCV_AB_1|HBSAG_1|HBEAG_1|ANTI_HBS_AB_1|ANTI_HBC_AB_1|ANTI_HDV_AB_1",colnames(TCM_Anova))]
+
+
+#做Anova +製圖輸出 (Anova_table_export.R取自Github上別人提供的程式碼)-------------------------------------------------
+source("Anova_table_export.R")
+#open PDF
+pdf(file = "C:\\R\\LS305中醫\\TCM_anova_result.pdf",
+    width = 10,
+    height = 5)
+
+#running plots
+for (i in c(1:79)){
+  TCM_Anova[,i+8] <- as.numeric(as.character(TCM_Anova[,i+8]))
+  TCM_Anova[,i+8][is.na(TCM_Anova[,i+8]) | TCM_Anova[,i+8]=="Inf"] = NA
+  model1 <- summary(aov(TCM_Anova[,i+8] ~ TCM_Anova$Yin_def*TCM_Anova$Yang_def*TCM_Anova$Phlegm_stasis ))
+  DisplayAnovaSummary(model_summary_object = model1, title = names(TCM_Anova[i+8]), title_font_size = 16,footnote = "")
+}
+
+
+#turn off PDF plotting
+dev.off() 
+#ERROR:Error in lm.fit(x, y, offset = offset, singular.ok = singular.ok, ...) : 
+#NA/NaN/Inf 出現於 'y'
+#TCM_ANOVA:c30=VC_PRED,變數是chr
+
+#Error in if ((model_summary$Pr..F.)[x] < 0.001) { : 
+#missing value where TRUE/FALSE needed   
+# TCM_ANOVA$MVV，值皆為NA
+
+# Error in lm.fit(x, y, offset = offset, singular.ok = singular.ok, ...) : 
+#   NA/NaN/Inf 出現於 'y'
+# In addition: Warning message:
+#   In storage.mode(v) <- "double" : NAs introduced by coercion
+# 變數是chr，且只有1,2兩種數字
+#取出anova圖中的p value製成表格--------------------------------------------------------------------
+TCM_Anova_p_value <- data.frame(c(1,1,1,1,1,1,1,1))
+
+for (i in c(1:79)){
+  model1 <- summary(aov(TCM_Anova[,i+8] ~ TCM_Anova$Yin_def*TCM_Anova$Yang_def*TCM_Anova$Phlegm_stasis ))
+  TCM_Anova_p_value[,i] <- model1[[1]][["Pr(>F)"]]
+}
+
+#從TCM_Anova抓欄名(加欄名)
+TCM_Anova_name1 <- colnames(subset(TCM_Anova,
+                                   select = c(9:87)))
+colnames(TCM_Anova_p_value) <- TCM_Anova_name1
+
+#加列名
+TCM_Anova_name2 <- c("TCM_Anova$Yin_def","TCM_Anova$Yang_def","TCM_Anova$Phlegm_stasis","TCM_Anova$Yin_def:TCM_Anova$Yang_def",
+                     "TCM_Anova$Yin_def:TCM_Anova$Phlegm_stasis","TCM_Anova$Yang_def:TCM_Anova$Phlegm_stasis",
+                     "CM_Anova$Yin_def:TCM_Anova$Yang_def:TCM_Anova$Phlegm_stasis","Residuals")
+rownames(TCM_Anova_p_value) <- TCM_Anova_name2
+
+#t()矩陣欄列互換
+TCM_Anova_p_value <- data.frame(t(TCM_Anova_p_value))
+
+#加變相中文解釋
+TCM_Anova_p_value_meaning1 <- c("陰虛","陽虛","痰瘀","陰虛+陽虛",
+                                "陰虛+痰瘀","陽虛+痰瘀",
+                                "陰虛+陽虛+痰瘀","殘餘")
+TCM_Anova_p_value <- rbind(TCM_Anova_p_value_meaning1, TCM_Anova_p_value)
+
+TCM_Anova_p_value_meaning2 <- c(" ","身高","體重","身體質量指數","體脂肪率","腰圍","臀圍","腰臀比",
+                                "靜坐時第一次收縮壓","靜坐時第一次舒張壓",
+                                "靜坐時第二次收縮壓","靜坐時第二次舒張壓",
+                                "靜坐時第三次收縮壓","靜坐時第三次舒張壓",
+                                "靜坐時第一次心跳","靜坐時第二次心跳","靜坐時第三次心跳",
+                                "STIFFNESS INDEX骨硬度指數","T參數值","Z參數值","超音波速度",
+                                "寬頻超音波遞減值","肺活量","肺活量基準值","一回換氣量","預備呼氣量",
+                                "預備吸氣量","預備吸氣量+一回換氣量","肺活量/身高","努力性肺活量",
+                                "努力性肺活量基準值"," Zeor點至1秒經過時的呼出量（一秒量）",
+                                "一秒量基準值"," 1秒率，(一秒量/努力性肺活量)*100 "," 1秒率基準值",
+                                " 1秒率，(一秒量/肺活量)*100 "," FEV10_SVC基準值"," FEV10_SVC基準值",
+                                " (FEV1.0/VC_PRED)*100 ","最大中間呼氣量(努力性肺活量的75％到25％的平均量)",
+                                "最大中間呼氣量基準值","最大呼氣流量","最大呼氣流量基準值",
+                                "努力性肺活量75％時流量","努力性肺活量75％時流量的基準值",
+                                "努力性肺活量50％時流量","努力性肺活量50％時流量的基準值",
+                                "努力性肺活量25％時流量","努力性肺活量25％時流量的基準值",
+                                "努力性肺活量25％時流量/身高","努力性肺活量25％時流量/身高的基準值",
+                                " 1秒率，(一秒量/肺活量)*100 ",
+                                "紅血球","白血球","血小板","血紅素","血球比容","醣化血色素值",
+                                " C型肝炎抗體值"," B型肝炎表面抗原值"," B型肝炎e抗原值",
+                                " B型肝炎表面抗體值"," B型肝炎核心抗體值"," D型肝炎抗體值",
+                                "飯前血糖","總膽固醇","三酸甘油酯",
+                                "高密度酯蛋白膽固醇","低密度酯蛋白膽固醇","總膽紅素","白蛋白",
+                                "血清麩胺酸苯醋酸轉氨基酵素","血清麩胺酸丙酮酸轉氨基酵素",
+                                "γ －麩胺醯轉移酵素","甲型胎兒血清蛋白","血中尿素氮",
+                                "肌酸酐","尿酸","尿中微蛋白","尿中肌酸酐"
+)
+TCM_Anova_p_value <- cbind(TCM_Anova_p_value_meaning2, TCM_Anova_p_value)
+
+
 #做卡方檢定---------------------------------------------------------------------------------------------------------------------------
 
 use_vb <- c("SEX", 
@@ -187,7 +296,7 @@ use_T_test_meaning <- c("年齡","身高","體重","身體質量指數","體脂肪率","腰圍", "
                         "強制呼出時肺活量（努力性肺活量）", "Zeor點至1秒經過時的呼出量（一秒量）", "紅血球", "白血球", "血小板", "血紅素", "血球比容", "醣化血色素值",
                         "AC型肝炎抗體（值）", "B型肝炎表面抗原（值）","B型肝炎e抗原(值)", "B型肝炎表面抗體（值）","B型肝炎核心抗體（值）", "D型肝炎抗體（值）", "飯前血糖", 
                         "總膽固醇", "三酸甘油酯", "高密度酯蛋白膽固醇", "低密度酯蛋白膽固醇", "總膽紅素", "白蛋白", "血清麩胺酸苯醋酸轉氨基酵素", 
-                        "血清麩胺酸丙酮酸轉氨基酵素", "A58	γ －麩胺醯轉移酵素", "甲型胎兒血清蛋白", "血中尿素氮", "肌酸酐", "尿酸", 
+                        "血清麩胺酸丙酮酸轉氨基酵素", "γ －麩胺醯轉移酵素", "甲型胎兒血清蛋白", "血中尿素氮", "肌酸酐", "尿酸", 
                         "尿中微蛋白", "尿中肌酸酐")
 
 result_set <- data.frame(vb1 = NA, vb2 = NA, vb2_釋義 = NA, p_value = NA)
@@ -213,61 +322,7 @@ for (i in 1:length(use_T_test)){
 }
 
 
-#Anova 資料清洗---------------------------------------------------------------------------------------------
-source("C:\\Github\\LS305Anova_table_export.R")
-
-TCM_group <- TCM_group[,-grep("AGE|age_gruop",colnames(TCM_group))] #去除sex AGE age_group
-TCM_Anova_mergerdata<- merge(TCM_group, chiqTCM, by = "Release_No",all.x = TRUE  )
-TCM_Anova <- TCM_Anova_mergerdata[,-grep("X|TWB1_ID|TWB2_ID|SEX.y|體質",colnames(TCM_Anova_mergerdata))]
-TCM_Anova <- TCM_Anova[-6244,]
-
-#隔離無法ANOVA的資料
-TCM_Anova_failed <- subset(TCM_Anova,
-                           select = c("MVV","ANTI_HCV_AB_1","HBSAG_1","HBEAG_1",
-                                      "ANTI_HBS_AB_1","ANTI_HBC_AB_1","ANTI_HDV_AB_1")
-)
-TCM_Anova <- TCM_Anova[,-grep("MVV|ANTI_HCV_AB_1|HBSAG_1|HBEAG_1|ANTI_HBS_AB_1|ANTI_HBC_AB_1|ANTI_HDV_AB_1",colnames(TCM_Anova))]
-
-
 #做Anova +輸出-------------------------------------------------
-#open PDF
-pdf(file = "C:\\R\\LS305中醫\\TCM_anova_result.pdf",
-    width = 10,
-    height = 5)
-
-#running plots
-for (i in c(1:78)){
-  TCM_Anova[,i+8] <- as.numeric(as.character(TCM_Anova[,i+8]))
-  TCM_Anova[,i+8][is.na(TCM_Anova[,i+8]) | TCM_Anova[,i+8]=="Inf"] = NA
-  model1 <- summary(aov(TCM_Anova[,i+8] ~ TCM_Anova$Yin_def*TCM_Anova$Yang_def*TCM_Anova$Phlegm_stasis ))
-  DisplayAnovaSummary(model_summary_object = model1, title = names(TCM_Anova[i+8]), title_font_size = 16,footnote = "")
-}
-
-
-#turn off PDF plotting
-dev.off() 
-
-#取出anova圖中的p value製成表格--------------------------------------------------------------------
-TCM_Anova_p_value <- data.frame(c(1,1,1,1,1,1,1,1))
-
-for (i in c(1:78)){
-  model1 <- summary(aov(TCM_Anova[,i+8] ~ TCM_Anova$Yin_def*TCM_Anova$Yang_def*TCM_Anova$Phlegm_stasis ))
-  TCM_Anova_p_value[,i] <- model1[[1]][["Pr(>F)"]]
-}
-
-#從TCM_Anova抓欄名(加欄名)
-TCM_Anova_name1 <- colnames(subset(TCM_Anova,
-                                   select = c(9:86)))
-colnames(TCM_Anova_p_value) <- TCM_Anova_name1
-
-#加列名
-TCM_Anova_name2 <- c("TCM_Anova$Yin_def","TCM_Anova$Yang_def","TCM_Anova$Phlegm_stasis","TCM_Anova$Yin_def:TCM_Anova$Yang_def",
-                     "TCM_Anova$Yin_def:TCM_Anova$Phlegm_stasis","TCM_Anova$Yang_def:TCM_Anova$Phlegm_stasis",
-                     "CM_Anova$Yin_def:TCM_Anova$Yang_def:TCM_Anova$Phlegm_stasis","Residuals")
-rownames(TCM_Anova_p_value) <- TCM_Anova_name2
-
-#t()矩陣欄列互換
-TCM_Anova_p_value <- data.frame(t(TCM_Anova_p_value))
 
 #匯出檔案---------------------------------------------------------------------------------------------------------------------------------
 write.csv(result_set,file='C:\\R\\LS305中醫\\卡方結果.csv',fileEncoding = "Big5")
@@ -275,5 +330,5 @@ write.csv(result_set_T_test, file='C:\\R\\LS305中醫\\T-test.csv',fileEncoding = 
 write.csv(chiqTCM, file='C:\\R\\LS305中醫\\中醫體質清理完成之資料.csv',fileEncoding = "Big5")
 write.csv(TCM_Anova, file = 'C:\\R\\LS305中醫\\TCM_Anova.csv',fileEncoding = "Big5")
 #輸出表格
-write.csv(TCM_Anova_p_value, file='C:\\R\\LS305中醫\\TCM_Anova_p_value.csv',fileEncoding = "Big5")
-write.csv(TCM_Anova_p_value, file='C:\\R\\LS305中醫\\TCM_Anova_p_value.csv',fileEncoding = "Big5")
+write.xlsx(TCM_Anova_p_value, file='C:\\R\\TCM_Anova_p_value.xlsx',fileEncoding = "Big5")
+write.xlsx(TCM_Anova_p_value, file='C:\\R\\LS305中醫\\TCM_Anova_p_value.xlsx',fileEncoding = "Big5")
