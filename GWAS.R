878672843c48f655576687b0391b887fe0b34416
陳陳 <410477jimmy@gmail.com>
2023-01-16 11:08:15 +0800

    GWAS 作圖

--- GWAS.R
+++ GWAS.R
@@ -42,7 +42,8 @@ covar_cons_sex_age <- cbind.data.frame(rm.TCMmerge3$TWB1_ID, covar_cons_sex_age)
 names(covar_cons_sex_age) <- c("FID" ,"IID","Sex","Age" )
 
 #作圖-----------------------------------------------------------------------------------
-result <- read.table("GWAS_PCA_Cons.assoc.logistic", header = TRUE)
+setwd("C:\\R\\GWAS")
+result <- read.table("result.assoc.logistic", header = TRUE)
 pvalue <- result[, c("SNP", "CHR", "BP", "P")]
 #manhattan plot 
 CMplot(pvalue, plot.type = "m", LOG10 = TRUE, threshold = 1e-5, chr.den.col = NULL, 
@@ -52,19 +53,115 @@ CMplot(pvalue, plot.type = "q", conf.int.col = NULL, box = TRUE,
        file = "jpg", memo = "", dpi = 300, file.output = TRUE, verbose = FALSE)
 median((result$STAT)^2)/0.455
 
-#subset significant region for LocusZoom
+#subset significant region for LocusZoom----------------------------------------
 # find min p-value
 subset(pvalue, P == min(P))
-# subset region
-locus <- subset(pvalue, CHR == 10 & BP < 119529777 + 400000 & BP > 119529777 - 400000)
-write.table(locus, "locus_10.txt", append = FALSE, quote = FALSE, sep = "\t", 
+
+# Yin_def:find another site---------------------------------------
+Yin_def_locus <- subset(pvalue, P < 1E-5)
+
+#Yin_def subset region
+
+locus <- subset(pvalue, CHR == 1 & BP < 181508326 + 400000 & BP > 181508326 - 400000)
+write.table(locus, "locus_1_rs6681017.txt", append = FALSE, quote = FALSE, sep = "\t", 
             row.names = FALSE, col.names = TRUE)
 
-# find another site
-subset(pvalue, P < 1E-6)
-# subset region
-locus <- subset(pvalue, CHR == 2 & BP < 99982730 + 400000 & BP > 99982730 - 400000)
-write.table(locus, "locus_2.txt", append = FALSE, quote = FALSE, sep = "\t", 
+
+locus <- subset(pvalue, CHR == 1 & BP < 181509821 + 400000 & BP > 181509821 - 400000)
+write.table(locus, "locus_1_rs6685078.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+
+locus <- subset(pvalue, CHR == 2 & BP < 121306440 + 400000 & BP > 121306440 - 400000)
+write.table(locus, "locus_2_rs17050272.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+
+locus <- subset(pvalue, CHR == 5 & BP < 168212096 + 400000 & BP > 168212096 - 400000)
+write.table(locus, "locus_5_rs10475891.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+
+locus <- subset(pvalue, CHR == 10 & BP < 60793767 + 400000 & BP > 60793767 - 400000)
+write.table(locus, "locus_10_rs10826267.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+
+locus <- subset(pvalue, CHR == 10 & BP < 132345848 + 400000 & BP > 132345848 - 400000)
+write.table(locus, "locus_10_rs1334910.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+
+locus <- subset(pvalue, CHR == 14 & BP < 70839635 + 400000 & BP > 70839635 - 400000)
+write.table(locus, "locus_14_rs3742916.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+
+locus <- subset(pvalue, CHR == 17 & BP < 2859272 + 400000 & BP > 2859272 - 400000)
+write.table(locus, "locus_17_rs8077217.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+
+# Yang_def:find another site--------------------------------------
+Yang_def_locus <- subset(pvalue, P < 1E-5)
+# Yang_def subset region
+
+locus <- subset(pvalue, CHR == 2 & BP < 237554988 + 400000 & BP > 237554988 - 400000)
+write.table(locus, "locus_2_rs1796444.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+locus <- subset(pvalue, CHR == 8 & BP < 96221214 + 400000 & BP > 96221214 - 400000)
+write.table(locus, "locus_8_rs880642.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+locus <- subset(pvalue, CHR == 11 & BP < 92605599 + 400000 & BP > 92605599 - 400000)
+write.table(locus, "locus_11_rs77906267.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+locus <- subset(pvalue, CHR == 13 & BP < 94870765 + 400000 & BP > 94870765 - 400000)
+write.table(locus, "locus_13_rs1572050.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+locus <- subset(pvalue, CHR == 15 & BP < 38847023 + 400000 & BP > 38847023 - 400000)
+write.table(locus, "locus_15_rs117840856.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+locus <- subset(pvalue, CHR == 17 & BP < 10143039 + 400000 & BP > 10143039 - 400000)
+write.table(locus, "locus_17_rs6503300.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+
+locus <- subset(pvalue, CHR == 22 & BP < 26416570 + 400000 & BP > 26416570 - 400000)
+write.table(locus, "locus_22_rs1033589.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+
+# Phlegm_stasis:find another site------------------------------------
+Phlegm_stasis_locus <- subset(pvalue, P < 1E-5)
+
+# Phlegm_stasis subset region
+locus <- subset(pvalue, CHR == 5 & BP < 168212096 + 400000 & BP > 168212096 - 400000)
+write.table(locus, "locus_5_rs10475891.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+locus <- subset(pvalue, CHR == 8 & BP < 94168269 + 400000 & BP > 94168269 - 400000)
+write.table(locus, "locus_8_rs11994583.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+locus <- subset(pvalue, CHR == 8 & BP < 94169379 + 400000 & BP > 94169379 - 400000)
+write.table(locus, "locus_8_rs278568.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+locus <- subset(pvalue, CHR == 10 & BP < 115381747 + 400000 & BP > 115381747 - 400000)
+write.table(locus, "locus_10_rs868738.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+locus <- subset(pvalue, CHR == 14 & BP < 60209188 + 400000 & BP > 60209188 - 400000)
+write.table(locus, "locus_14_rs10138254.txt", append = FALSE, quote = FALSE, sep = "\t", 
+            row.names = FALSE, col.names = TRUE)
+
+locus <- subset(pvalue, CHR == 22 & BP < 26416570 + 400000 & BP > 26416570 - 400000)
+write.table(locus, "locus_22_rs1033589.txt", append = FALSE, quote = FALSE, sep = "\t", 
             row.names = FALSE, col.names = TRUE)
 
 
